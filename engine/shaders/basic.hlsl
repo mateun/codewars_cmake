713f9077b4bb9b3a1d0b2919e55301d5f4027544
Martin Gruscher <m.gruscher@ttech.at>
2017-05-16 22:29:45 +0200

    fix missing transformations in basic.hlsl; add TODO in cmakelists.txt

--- engine/shaders/basic.hlsl
+++ engine/shaders/basic.hlsl
@@ -16,8 +16,8 @@ VOut VShader(float4 position : POSITION, float2 tex : TEXCOORD0) {
 
 	VOut output;
 	output.pos = mul(position, worldm);
-	//output.pos = mul(output.pos, viewm);
-	//output.pos = mul(output.pos, projm);
+	output.pos = mul(output.pos, viewm);
+	output.pos = mul(output.pos, projm);
 	//output.pos = position;
 	output.tex = tex;
 	
