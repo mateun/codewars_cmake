c6d08dbe6f935c4bca491604a90e15e95753bbfa
Martin Gruscher <m.gruscher@ttech.at>
2017-06-05 11:56:03 +0200

    basic implementation of calling a c-function from python.

--- engine/include/py_embed.h
+++ engine/include/py_embed.h
@@ -1,4 +1,18 @@
 #pragma once
 #include <Python.h>
 
+/// Call to python
 void callPyFunc();
+
+
+/// Callable from python
+
+// Module Initializer
+PyObject* PyInit_emb(void);
+
+/**
+	Get health for player
+	params: 
+		int => player number
+*/
+PyObject* emb_getHealth(PyObject* self, PyObject* args);
--- engine/src/py_embed.cpp
+++ engine/src/py_embed.cpp
@@ -5,15 +5,29 @@ static PyObject *pName, *pModule, *pDict, *pFunc;
 static PyObject *pArgs, *pValue;
 
 void callPyFunc() {
-	//PyImport_AppendInittab("emb", &PyInit_emb);
-	Py_SetProgramName(L"TESTPYTHON");
+	
+	//Py_SetProgramName(L"TESTPYTHON");
+	int res = PyImport_AppendInittab("codewars", &PyInit_emb);
 	Py_Initialize();
+	
 	//PySys_SetPath(L"C:/Users/martin/Documents/Projects/C++/codewars/build/win32_dist/x86-Debug/Debug");
 	PySys_SetPath(L"./py_scripts");
 	//PyObject* sysPath = PySys_GetObject("path");
 	//PyList_Append(sysPath, PyUnicode_FromString("."));
 	pName = PyUnicode_DecodeFSDefault("testprog");
-	pModule = PyImport_Import(pName);
+	try {
+		pModule = PyImport_Import(pName);
+	} catch (...) {
+		PyObject *ptype, *pvalue, *ptraceback;
+		PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+		//pvalue contains error message
+		//ptraceback contains stack snapshot and many other information
+		//(see python traceback structure)
+
+		//Get error message
+		//char *pStrErrorMessage = PyBytes_FromString(pvalue);
+		
+	}
 	Py_DECREF(pName);
 
 	if (pModule == nullptr) {
@@ -32,4 +46,42 @@ void callPyFunc() {
 	}
 	
 	Py_FinalizeEx();
+}
+
+/// Embedding stuff
+PyMethodDef EmbMethods[] = {
+	{"get_player_health", emb_getHealth, METH_VARARGS,
+	"Return the current health value of the player given by its number."},
+	{NULL, NULL, 0, NULL}
+};
+
+PyModuleDef EmbModule = {
+	PyModuleDef_HEAD_INIT, "codewars", NULL, -1, EmbMethods,
+	//NULL, NULL, NULL, NULL
+};
+
+PyObject* PyInit_emb(void) {
+	try {
+		PyObject* m = PyModule_Create(&EmbModule);
+		if (m) {
+			OutputDebugString("loaded python module\n");
+			return m;
+		}
+	}
+	catch (...) {
+		OutputDebugString("caught exception while calling PyModuleCreate\n");
+	}
+}
+
+PyObject* emb_getHealth(PyObject* self, PyObject* args) {
+	OutputDebugString("emb_getHealth called from python!\n");
+	int playerNr = 0;
+	int playerHealth = 100;
+	if (!PyArg_ParseTuple(args, "i", &playerNr)) {
+		return nullptr;
+	}
+
+	// TODO make fake impl real
+	// e.g. lookup health for player somewhere
+	return PyLong_FromLong(playerHealth);
 }
